9f3d9ad708a9c0566b1f0025ecb37ec38360e3d0
Emment Yamikani Banda <42191820+Emment-Yamikani@users.noreply.github.com>
2023-10-18 07:32:47 +0200

    Update preempt.h
    Removed macro equivalents of pushcli(void) and popcli(void).

--- kernel/include/sync/preempt.h
+++ kernel/include/sync/preempt.h
@@ -24,23 +24,3 @@ static inline void popcli(void) {
         sti();
     }
 }
-
-/*
-#define pushcli() ({               \
-    uint64_t intena = is_intena(); \
-    cli();                         \
-    if (intena)                    \
-        cpu->intena = intena;      \
-    cpu->ncli++;                   \
-})
-
-#define popcli() ({                                                                                \
-    assert_msg(!is_intena(), "%s:%d: error: interrupts enabled before popcli()!", __FILE__, __LINE__); \
-    assert_msg((cpu->ncli >= 1), "%s:%d: error: ncli == %d\n", __FILE__, __LINE__, cpu->ncli);         \
-    if ((--cpu->ncli == 0) && cpu->intena)                                                         \
-    {                                                                                              \
-        cpu->intena = 0;                                                                           \
-        sti();                                                                                     \
-    }                                                                                              \
-})
-*/
